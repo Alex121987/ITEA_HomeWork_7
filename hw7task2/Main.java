01e5b34d4ae2f8649a7598f6eb6808e35cac1052
“Alexander <alexander.grigoriev.121987@gmail.com>
2023-10-16 14:40:42 +0300

    Task 2

--- hw7task2/Main.java
+++ hw7task2/Main.java
@@ -7,6 +7,18 @@ public class Main {
         myDictionary.put(1, "Alex");
         myDictionary.put(2, "Bob");
         myDictionary.put(3, "Mitch");
+        myDictionary.put(4, "Sam");
+        myDictionary.put(5, "Luke");
+        myDictionary.put(6, "Marcus");
+        myDictionary.put(7, "Mat");
+        myDictionary.put(8, "Brad");
+        myDictionary.put(9, "Pearce");
+        myDictionary.put(10, "Robby");
+        myDictionary.put(11, "Ien");
+        myDictionary.put(12, "Ken");
+        myDictionary.put(13, "Frank");
+        myDictionary.put(14, "Freddy");
+        myDictionary.put(15, "Antonio");
         System.out.println(myDictionary);
 
         System.out.println(myDictionary.getSize());
